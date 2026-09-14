3608d9da9f9be29e159417a21dc4bd6637a24c04
Vinicius Ribeiro <viniciusgn.ribeiro@gmail.com>
2026-09-14 19:31:20 -0300

    feat/test: complete core game engine with meld play, initial meld rules and automated tests

--- src/rummikub/core/game.py
+++ src/rummikub/core/game.py
@@ -1,5 +1,5 @@
 from itertools import cycle
-from .board import Board
+from .board import Board, Meld
 from .bag import TileBag
 from .player import Player
 
@@ -16,13 +16,18 @@ class Game:
         self.current_player = next(self._iter_players)
 
         self.winner: Player | None = None
+        self._current_player_rack_snapshot = None
+        self._current_player_has_initial_meld_state = False
 
     def next_player(self):
         self.current_player = next(self._iter_players)
+        self.start_turn()
 
     def start_game(self):
         for player in self.players:
             player.add_tiles(self.bag.draw_many(14))
+
+        self.start_turn()
 
     def player_draw_tile(self):
         self.current_player.add_tiles([self.bag.draw()])
@@ -34,3 +39,50 @@ class Game:
                 self.winner = player
                 return True
         return False
+
+    def start_turn(self):
+        self.board.take_snapshot()
+        self._current_player_rack_snapshot = list(self.current_player.rack)
+        self._current_player_has_initial_meld_state = self.current_player.has_initial_meld
+
+    def rollback_turn(self):
+        self.board.rollback()
+        if self._current_player_rack_snapshot is not None:
+            self.current_player.rack = list(self._current_player_rack_snapshot)
+            self._current_player_rack_snapshot = None
+            self.current_player.has_initial_meld = self._current_player_has_initial_meld_state
+
+    def commit_turn(self):
+        self.board.commit()
+        self._current_player_rack_snapshot = None
+
+    def play_melds(self, new_melds: list[Meld]):
+        for meld in new_melds:
+            # if not all([tile in self.current_player.rack for tile in meld.tiles]):
+            #     raise ValueError("Player does not have the intended Tile in Rack")
+
+            for tile in meld.tiles:
+                if tile not in self.current_player.rack:
+                    self.rollback_turn()
+                    raise ValueError("Player does not have the intended Tile in Rack")
+
+                self.current_player.remove_tile(tile)
+
+            self.board.add_meld(meld)
+
+        if not self.current_player.has_initial_meld:
+            total_points = sum(meld.points for meld in new_melds)
+            if total_points < 30:
+                self.rollback_turn()
+                raise ValueError("Initial meld must be at least 30 points")
+            else:
+                self.current_player.has_initial_meld = True
+
+        if not self.board.is_valid():
+            self.rollback_turn()
+            raise ValueError("Invalid final table")
+
+        self.commit_turn()
+        self.has_winner()
+        self.next_player()
+
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,7 @@
 import pytest
-
-from src.rummikub.core.game import Game
+from rummikub.core.game import Game
+from rummikub.core.meld import Group
+from rummikub.core.tile import Tile, Color
 
 def test_game_initial_state():
     game = Game(["Joao", "Luiza"])
@@ -50,4 +51,77 @@ def test_has_winner():
 
     assert game.has_winner()
     assert game.winner == game.players[0]
+
+def test_rollback_turn_restores_board_and_rack():
+    game = Game(["Joao", "Luiza"])
+    game.start_game()
+
+    first_player = game.current_player
+
+    first_player.rack.pop()
+    first_player.rack.pop()
+    first_player.rack.pop()
+
+    assert len(first_player.rack) == 11
+
+    game.board.add_meld(Group([Tile(Color.BLACK, 6)]))
+
+    assert len(game.board) == 1
+    assert not game.board.is_valid()
+
+    game.rollback_turn()
+
+    assert len(first_player.rack) == 14
+    assert len(game.board) == 0
+    assert game.board.is_valid()
+
+def test_play_melds_valid_initial_meld():
+    game = Game(["Joao", "Luiza"])
+    meld = Group([Tile(Color.RED, 10), Tile(Color.BLUE, 10), Tile(Color.BLACK, 10)])
+    game.current_player.rack = list(meld.tiles)
+
+    game.start_turn()
+    game.play_melds([meld])
+
+    assert len(game.players[0].rack) == 0
+    assert meld in game.board.melds
+    assert game.players[0].has_initial_meld
+    assert game.current_player.name == "Luiza"
+
+def test_play_melds_initial_meld_less_than_30_points_fails():
+    game = Game(["Joao", "Luiza"])
+    meld = Group([Tile(Color.RED, 7), Tile(Color.BLUE, 7), Tile(Color.BLACK, 7)])
+    game.current_player.rack = list(meld.tiles)
+
+    game.start_turn()
+
+    with pytest.raises(ValueError, match="30 points"):
+        game.play_melds([meld])
+
+    assert not game.players[0].has_initial_meld
+    assert game.players[0].rack == list(meld.tiles)
+    assert len(game.board) == 0
+
+def test_play_melds_with_tiles_not_in_rack_fails():
+    game = Game(["Joao", "Luiza"])
+    meld = Group([Tile(Color.RED, 1), Tile(Color.BLUE, 1), Tile(Color.BLACK, 1)])
+    game.current_player.rack = list(meld.tiles)
+
+    game.start_turn()
+    with pytest.raises(ValueError, match="Rack"):
+        game.play_melds([Group([Tile(Color.RED, 10), Tile(Color.BLUE, 10), Tile(Color.BLACK, 10)])])
+
+    assert game.players[0].rack == list(meld.tiles)
+    assert len(game.board) == 0
+
+def test_play_melds_winning_game():
+    game = Game(["Joao", "Luiza"])
+    meld = Group([Tile(Color.RED, 10), Tile(Color.BLUE, 10), Tile(Color.BLACK, 10)])
+    game.current_player.rack = list(meld.tiles)
+
+    game.start_turn()
+    game.play_melds([meld])
+
+    assert game.has_winner()
+    assert game.winner.name == "Joao"
 
